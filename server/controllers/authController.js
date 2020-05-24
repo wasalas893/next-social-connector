9bf5db5d84b749e1aa798cd2e25b15831fb59c32
wasalas <wasalas893@gmail.com>
2020-05-24 18:55:52 +0530

    create backend singnin logout

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,9 @@
 const mongoose=require('mongoose');
+
+
 const User=mongoose.model('User');
+
+const passport=require('passport');
 
 exports.validateSignup = (req,res,next) => {
     req.sanitizeBody('name')
@@ -41,7 +45,7 @@ exports.signup = async (req,res) => {
         if(err){
             return res.status(500).send(err.message);
         }
-        res.json(user);
+        res.json(user.name);
     })
 
 
@@ -50,8 +54,33 @@ exports.signup = async (req,res) => {
 
 };
 
-exports.signin = () => {};
+exports.signin = (req,res,next) => {
+    passport.authenticate('local',(err,user,info)=>{
+        if(err){
+            return res.status(500).json(err.message);
+        }
+        if(!user){
+            return res.status(400).json(info.message);
+        }
+        req.logIn(user,err=>{
+            if(err){
+                return res.status(500).json(err.message);
+            }
+            res.json(user);
+        })
+    })(req,res,next);
+};
 
-exports.signout = () => {};
+exports.signout = (req,res) => {
+    res.clearCookie("next-cookie.sid");
+    req.logout();
+    res.json({ message:"You are now signed out!"});
 
-exports.checkAuth = () => {};
+};
+
+exports.checkAuth = (req,res,next) => {
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/signin');
+};
