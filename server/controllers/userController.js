0908643fe5b6619e9ebb87ec7fc2086af708480c
wasalas <wasalas893@gmail.com>
2020-05-25 10:16:52 +0530

    create backend api/users/follow and api/users/unfollow user routers

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -59,10 +59,46 @@ exports.deleteUser = async(req,res) => {
     res.json(deletedUser)
 };
 
-exports.addFollowing = () => {};
+exports.addFollowing = async(req,res,next) => {
+    const { followId }=req.body;
 
-exports.addFollower = () => {};
+    await User.findOneAndUpdate(
+        { _id: req.user._id },
+        { $push: { following: followId } }
+    )
+    next();
+};
 
-exports.deleteFollowing = () => {};
+exports.addFollower = async(req,res) => {
 
-exports.deleteFollower = () => {};
+    const {followId }=req.body;
+
+    const user=await User.findOneAndUpdate(
+        { _id: followId },
+        { $push: { followers: req.user._id } },
+        { new: true }
+    )
+    res.json(user);
+};
+
+exports.deleteFollowing = async(req,res,next) => {
+    const { followId }=req.body;
+
+    await User.findOneAndUpdate(
+        { _id: req.user._id },
+        { $pull: { following: followId } }
+    )
+    next();
+
+};
+
+exports.deleteFollower = async(req,res) => {
+    const {followId }=req.body;
+
+    const user=await User.findOneAndUpdate(
+        { _id: followId },
+        { $pull: { followers: req.user._id } },
+        { new: true }
+    )
+    res.json(user);
+};
--- server/routes/index.js
+++ server/routes/index.js
@@ -28,6 +28,19 @@ router.get("/api/auth/signout", authController.signout);
  */
 router.param("userId", userController.getUserById);
 
+router.put(
+  "/api/users/follow",
+  authController.checkAuth,
+  catchErrors(userController.addFollowing),
+  catchErrors(userController.addFollower)
+);
+router.put(
+  "/api/users/unfollow",
+  authController.checkAuth,
+  catchErrors(userController.deleteFollowing),
+  catchErrors(userController.deleteFollower)
+);
+
 router
   .route("/api/users/:userId")
   .get(userController.getAuthUser)
@@ -50,18 +63,7 @@ router.get(
   catchErrors(userController.getUserFeed)
 );
 
-router.put(
-  "/api/users/follow",
-  authController.checkAuth,
-  catchErrors(userController.addFollowing),
-  catchErrors(userController.addFollower)
-);
-router.put(
-  "/api/users/unfollow",
-  authController.checkAuth,
-  catchErrors(userController.deleteFollowing),
-  catchErrors(userController.deleteFollower)
-);
+
 
 /**
  * POST ROUTES: /api/posts
